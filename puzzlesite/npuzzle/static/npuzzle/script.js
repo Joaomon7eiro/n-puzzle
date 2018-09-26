8117121cf9a493bb1697377c8ec735f380582615
João Vitor Monteiro Nandes <joao_agrom@hotmail.com>
2018-09-26 10:59:21 -0300

    game on front end finished

--- puzzlesite/npuzzle/static/npuzzle/script.js
+++ puzzlesite/npuzzle/static/npuzzle/script.js
@@ -2,9 +2,19 @@ function alerta(matriz){
     alert(matriz)
 }
 
-function move(row, col, matriz){
-    console.log(matriz)
-    return
+function move(row, col, matrizAsArray, dimension){
+
+    matriz = []
+    row_values = []
+    for(let i= 0, j=0 ; i <= matrizAsArray.length ; i++, j++){
+        if(j == dimension){
+            matriz.push(row_values)
+            row_values = []
+            j = 0
+        }
+        row_values.push(matrizAsArray[i])
+    }
+
     let distance = 0
     for(let i = 0; i < matriz.length; i++) {
         for(let j=0; j < matriz[0].length; j++){
@@ -31,20 +41,21 @@ function move(row, col, matriz){
 
 
     if(distance > 1){
-        alert("ruim")
+        console.log("movimento invalido")
     }else{
         matriz[row0][col0] = matriz[row][col]
         matriz[row][col] = 0
 
         console.log(matriz)
+        appendNewMatriz(matriz, dimension)
     }
 }
 
 function createMatriz(){
     let dimension = $("#dimension").val()
 
-    $("#create").remove()
-    $("#dimension").remove()
+    $("#stepOne").remove()
+
     if(!dimension){
         dimension = 0;
     }
@@ -76,12 +87,27 @@ function appendNewMatriz(matriz, dimension){
 
     table = $("#table")
 
-    alert(matriz)
+    console.log(matriz)
 
     for(let i = 0; i< matriz.length ; i++){
         table.append("<tr id='row"+i+"'/>")
         for(let j = 0; j < matriz.length; j++){
-            $("#row"+i).append(`<td id='col"+j+"' onclick='move(${i},${j}, ${matriz});' >`+matriz[i][j]+`</td>`)
+            $("#row"+i).append(`<td id='col${j}' onclick='move(${i},${j},[${matriz}], ${dimension});' >`+matriz[i][j]+`</td>`)
         }
     }
+
+    container.append(
+    `<form action="#" method="post">
+       <input type="hidden" class="form-control" id="matriz" placeholder="" value="[${matriz}]"
+              name='matriz'>
+       <input type="submit" value="Resolver">
+    </form>`)
+
 }
+
+
+var inputElem = document.createElement('input');
+inputElem.type = 'hidden';
+inputElem.name = 'csrfmiddlewaretoken';
+inputElem.value = '{{ csrf_token }}';
+selectform.appendChild(inputElem);
